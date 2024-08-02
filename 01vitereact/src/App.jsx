3b6d120d67bf8e0b5e8c8cd4b2d71bfca6fbc7a6
rushikesh109 <mangrulerushikesh2003@gmail.com>
2024-08-02 14:58:21 +0530

    react structure

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <h1>chai aur react with vite | Rushikesh</h1>
+   
+  )
+}
+
+export default App
---
+++ 01vitereact/src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+function MyApp(){
+  return(
+    <div>
+      <h1>Custom App | rushi</h1>
+    </div>
+  )
+
+}
+ReactDOM.createRoot(document.getElementById('root')).render(
+   < MyApp/>
+ )
