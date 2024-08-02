1b9c2f9685ff3a1dfcb9f045230d8bede95be834
rushikesh109 <mangrulerushikesh2003@gmail.com>
2024-08-02 15:51:58 +0530

    counters in react

---
+++ 02_counter/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let [counter, setCounter] = useState(0)
+
+  const addValue = () => {   
+
+    if(counter<20 ){               
+      counter = counter + 1
+      setCounter(counter)
+    }
+  console.log("Clicked",counter);  
+}
+
+const removeValue = () => {
+     if(counter>0 ){
+      counter = counter - 1
+      setCounter(counter)
+    }
+  console.log("Clicked",counter);  
+}
+  return (
+    <>
+      <h1>Chai aur react</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button
+      onClick={addValue}
+      >Add value {counter}</button> 
+      <br />
+      <button
+      onClick={removeValue}
+      >remove value {counter}</button>
+      <p>footer: {counter}</p>
+    </>
+  )
+}
+
+export default App
